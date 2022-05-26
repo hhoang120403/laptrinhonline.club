faec99fd5f725e205298340e6578d2c4aa6c2d1c
hhoang_1204 <101880686+hhoang120403@users.noreply.github.com>
2022-05-26 22:26:08 +0700

    Create choibai3nguoi.cpp

---
+++ choibai3nguoi.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    string a,b,c;
+    cin >> a >> b >> c;
+    int m=0,n=0,k=0,check=1,res,i=1;
+    while(check){
+    	if(i==1){
+    		while(a[m]=='a'){
+    			m++;
+			}
+			if(a[m]=='b') i = 2;
+			else i = 3;
+			m++;
+			if(m>a.size()){
+				check = 0;
+				res=m;
+				break;
+			}
+		}
+		else if(i==2){
+    		while(b[n]=='b'){
+    			n++;
+			}
+			if(b[n]=='a') i = 1;
+			else i = 3;
+			n++;
+			if(n>b.size()){
+				check = 0;
+				res=n;
+				break;
+			}
+		}
+		else{
+    		while(c[k]=='c'){
+    			k++;
+			}
+			if(c[k]=='a') i = 1;
+			else i = 2;
+			k++;
+			if(k>c.size()){
+				check = 0;
+				res = k;
+				break;
+			}
+		}
+	}
+	if(res==m) cout << "A";
+	else if(res==n) cout << "B";
+	else cout << "C";
+}
