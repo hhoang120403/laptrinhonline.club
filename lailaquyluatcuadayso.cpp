79c4bb6193b6795e0e7a34d2d96e01e67770d653
hhoang_1204 <101880686+hhoang120403@users.noreply.github.com>
2022-06-09 18:06:03 +0700

    Create lailaquyluatcuadayso.cpp

---
+++ lailaquyluatcuadayso.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+int main(){
+   	int t;
+   	cin >> t;
+   	long long n;
+   	while(t--){
+   		cin >> n;
+   		cout << (n-1)*n/2 + 1 << endl;
+	}
+}
