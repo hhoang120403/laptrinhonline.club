7e796b4112018650ac2969c653ecb447db4fa62c
hhoang_1204 <101880686+hhoang120403@users.noreply.github.com>
2022-07-28 10:11:50 +0700

    QHD

---
+++ Leothang.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int dp[21] = {};
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i=3;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+    cout << dp[n];
+}
